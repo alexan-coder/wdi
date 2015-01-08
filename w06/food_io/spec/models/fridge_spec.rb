33a86b79c5dc989d68bf6318cdc6867454b02606
mattcho <dongukcho@gmail.com>
2015-01-08 15:08:32 -0500

    before moving to Sean

--- w06/food_io/spec/models/fridge_spec.rb
+++ w06/food_io/spec/models/fridge_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+### Fridge Spec
+# * has a location
+# * has a brand
+# * has a size in cubic feet
+# * has food in it
+# * has drinks in it
+
 describe Fridge do
 	describe "::new" do # ::new notation for testing class method
 		it "can be instantiated" do
@@ -10,3 +17,10 @@ describe Fridge do
 	it { should validate_presence_of(:location) }
 	it { should validate_presence_of(:brand) }
 end
+
+# RSpec.describe Fridge, :type => :model do
+# 	it "has a location" do
+# 		fridge = Fridge.new
+# 		expect(fridge.valid?).to eq(true)
+# 	end
+# end
